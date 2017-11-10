95b3ba662f3ee71ae066644ff2640a83ae710d46
simon <guoying19878@163.com>
2017-11-10 14:54:38 +0800

    commit

--- Admin.java
+++ Admin.java
@@ -1,7 +1,8 @@
 
 public  class  Admin｛
      public  static  void  main(){
-		 
+		  System.out.println("this  is  git  file  test");
+		 System.out.println("this  is  git  file  test");
 		 System.out.println("this  is  git  file  test")
 		 
 	 }
